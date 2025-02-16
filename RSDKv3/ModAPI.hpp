52f28f1433716161c5f3e3f1aaaac191ce8a6225
aloaf <toastedhuman12345@gmail.com>
2025-02-16 17:38:26 +0900

    dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia dementia

--- RSDKv3/ModAPI.hpp
+++ RSDKv3/ModAPI.hpp
@@ -7,7 +7,7 @@
 #include <unordered_map>
 #include <tinyxml2.h>
 
-#define PLAYER_MAX (0x10)
+#define PLAYERNAME_COUNT (0x10)
 
 struct ModInfo {
     std::string name;
@@ -17,8 +17,9 @@ struct ModInfo {
     std::map<std::string, std::string> fileMap;
     std::string folder;
     bool useScripts;
-    bool disableFocusPause;
+    int disableFocusPause;
     bool redirectSave;
+    bool disableSaveIniOverride;
     std::string savePath;
     bool active;
 };
@@ -29,29 +30,32 @@ extern int activeMod;
 extern char modsPath[0x100];
 
 extern bool redirectSave;
-extern char savePath[0x100];
+extern bool disableSaveIniOverride;
 
 extern char modTypeNames[OBJECT_COUNT][0x40];
 extern char modScriptPaths[OBJECT_COUNT][0x40];
 extern byte modScriptFlags[OBJECT_COUNT];
 extern byte modObjCount;
 
-extern char playerNames[PLAYER_MAX][0x20];
+extern char playerNames[PLAYERNAME_COUNT][0x20];
 extern byte playerCount;
 
 inline void SetActiveMod(int id) { activeMod = id; }
 
-void initMods();
-bool loadMod(ModInfo *info, std::string modsPath, std::string folder, bool active);
-void scanModFolder(ModInfo *info);
-void saveMods();
+void InitMods();
+bool LoadMod(ModInfo *info, std::string modsPath, std::string folder, bool active);
+void ScanModFolder(ModInfo *info);
+void SaveMods();
 
 int OpenModMenu();
 
 void RefreshEngine();
 
-int GetSceneID(byte listID, const char *sceneName);
+#endif
 
+#if RETRO_USE_MOD_LOADER || !RETRO_USE_ORIGINAL_CODE
+extern char savePath[0x100];
+int GetSceneID(byte listID, const char *sceneName);
 #endif
 
 #endif
