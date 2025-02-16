c8e2ce0ae4ee4a0cbebc44e5196f929c27560cb6
aloaf <toastedhuman12345@gmail.com>
2025-02-16 12:23:07 +0900

    one more line

--- RSDKv3/Video.hpp
+++ RSDKv3/Video.hpp
@@ -14,11 +14,12 @@ extern const THEORAPLAY_VideoFrame *videoVidData;
 extern const THEORAPLAY_AudioPacket *videoAudioData;
 extern THEORAPLAY_Io callbacks;
 
-extern byte videoData;
+extern byte videoSurface;
 extern int videoFilePos;
-extern bool videoPlaying;
+extern int videoPlaying; // 0 = not playing, 1 = playing OGV, 2 = playing RSV
 extern int vidFrameMS;
 extern int vidBaseticks;
+extern float videoAR;
 
 void PlayVideoFile(char *filepath);
 void UpdateVideoFrame();
